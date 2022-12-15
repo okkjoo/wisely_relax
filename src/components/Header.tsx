739b5a8adb56243a94f94f8a008a027d44d84740
okkjoo <keyjo_zhou@m.scnu.edu.cn>
2022-12-15 12:58:47 +0800

    feat: header 样式

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,12 @@
 import styles from "styles/app.module.scss";
 
 const Header: React.FC = () => {
-  return <div className={styles.header}></div>;
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}> 休息time</div>
+      <div className={styles.introduce}>关于我们 | 反馈</div>
+      <div className={styles.close}>❎</div>
+    </header>
+  );
 };
 export default Header;
