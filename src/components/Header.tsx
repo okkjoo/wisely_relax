a79d55554f35aff84f9282258a463a316fa8191e
okkjoo <keyjo_zhou@m.scnu.edu.cn>
2022-12-16 12:42:51 +0800

    feat: 完善点击样式、clickIntro逻辑

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,19 @@
 import styles from "styles/app.module.scss";
 
 const Header: React.FC = () => {
+  const clickIntro = () => {
+    window.open("https://github.com/okkjoo/wisely_relax");
+  };
+  const clickClose = () => {};
   return (
     <header className={styles.header}>
       <div className={styles.logo}> 休息time</div>
-      <div className={styles.introduce}>关于我们 | 反馈</div>
-      <div className={styles.close}>❎</div>
+      <div className={styles.introduce} onClick={clickIntro}>
+        关于我们 | 反馈
+      </div>
+      <div className={styles.close} onClick={clickClose}>
+        ❎
+      </div>
     </header>
   );
 };
