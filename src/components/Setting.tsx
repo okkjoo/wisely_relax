385e7259ed3e16afea4c04008ec8c1174c65fd7f
okkjoo <keyjo_zhou@m.scnu.edu.cn>
2022-12-17 14:19:21 +0800

    feat: setting restSelect List

--- src/components/Setting.tsx
+++ src/components/Setting.tsx
@@ -2,12 +2,30 @@ import { useState } from "react";
 import styles from "styles/app.module.scss";
 const Setting: React.FC = () => {
   const [archiveFlag, setArchiveFlag] = useState(true);
+  const [textFoucsFlag, setTextFoucsFlag] = useState(false);
   const [text, setText] = useState("");
+  const [inputs, setInputs] = useState(["do1", "do2", "do3", "do4", "do5"]);
   const handleArchiveFlag = () => {
     setArchiveFlag(!archiveFlag);
   };
   const handleChangeText = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setText(e.target.value);
+  };
+  const handleChangeTextFocus = (flag: boolean) => () => {
+    setTextFoucsFlag(flag);
+  };
+  const handleChangeInput =
+    (idx: number) => (e: React.ChangeEvent<HTMLInputElement>) => {
+      const newClocks = [...inputs];
+      newClocks.splice(idx, 1, e.target.value);
+      setInputs(newClocks);
+    };
+  const thing2cnt = new Map();
+  const handleClickAddBtn = (idx: number) => () => {
+    const thing = inputs[idx];
+    if (!thing2cnt.get(thing)) thing2cnt.set(thing, 1);
+    thing2cnt.set(thing, thing2cnt.get(thing) + 1);
+    console.log(thing2cnt);
   };
   return (
     <div className={styles.middle}>
@@ -25,10 +43,34 @@ const Setting: React.FC = () => {
           className={styles.archiveText}
           defaultValue={text}
           onChange={handleChangeText}
+          onFocus={handleChangeTextFocus(true)}
+          onBlur={handleChangeTextFocus(false)}
+          spellCheck="false"
         ></textarea>
       ) : (
         <></>
       )}
+      <div
+        className={[styles.restSelect, textFoucsFlag ? styles.short : ""].join(
+          " "
+        )}
+      >
+        <h2>休息选择</h2>
+        {inputs.map((input, idx) => (
+          <div className={styles.inputItem} key={idx}>
+            <input
+              className={styles.restSelectItem}
+              type="text"
+              defaultValue={input}
+              onChange={handleChangeInput(idx)}
+              spellCheck="false"
+            ></input>
+            <div className={styles.itembtn} onClick={handleClickAddBtn(idx)}>
+              ✅
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
