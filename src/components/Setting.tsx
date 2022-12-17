627b77d558b5d5b3be44783d88583b10b121e2d4
okkjoo <keyjo_zhou@m.scnu.edu.cn>
2022-12-17 11:44:43 +0800

    feat: setting archiveText

--- src/components/Setting.tsx
+++ src/components/Setting.tsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
 import styles from "styles/app.module.scss";
 const Setting: React.FC = () => {
-  return <div className={styles.middle}></div>;
+  const [archiveFlag, setArchiveFlag] = useState(true);
+  const [text, setText] = useState("");
+  const handleArchiveFlag = () => {
+    setArchiveFlag(!archiveFlag);
+  };
+  const handleChangeText = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(e.target.value);
+  };
+  return (
+    <div className={styles.middle}>
+      <h2>个性化设置</h2>
+      <input
+        type="checkbox"
+        value="archiveFlag"
+        id="archiveFlag"
+        onChange={handleArchiveFlag}
+        checked={archiveFlag}
+      />
+      需要工作存档
+      {archiveFlag ? (
+        <textarea
+          className={styles.archiveText}
+          defaultValue={text}
+          onChange={handleChangeText}
+        ></textarea>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
 };
 export default Setting;
